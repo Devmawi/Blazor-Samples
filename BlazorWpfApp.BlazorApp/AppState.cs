00cce1037a060abc9296ed12eb2f0fa7fa377996
Dev <dev@dev.dev>
2021-12-11 20:20:47 +0100

    Add project files.

---
+++ BlazorWpfApp.BlazorApp/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWpfApp.BlazorApp
+{
+	public class AppState
+	{
+		public int Counter { get; set; } = 0;
+	}
+}
---
+++ BlazorWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BlazorWpfApp.BlazorApp;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlazorWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly AppState _appState = new();
+        public MainWindow()
+        {
+            var services1 = new ServiceCollection();
+            services1.AddBlazorWebView();
+            services1.AddSingleton<AppState>(_appState);
+            Resources.Add("services1", services1.BuildServiceProvider());
+
+            InitializeComponent();
+        }
+    }
+
+  
+}
