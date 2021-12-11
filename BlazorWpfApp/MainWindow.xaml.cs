0fa1fa0ac03f5959c3c9e3dcd164d88f789372da
Dev <dev@dev.dev>
2021-12-11 21:54:22 +0100

    edit

--- BlazorWpfApp/MainWindow.xaml.cs
+++ BlazorWpfApp/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace BlazorWpfApp
         public MainWindow()
         {
             var services1 = new ServiceCollection();
-            services1.AddBlazorWebView();
+            services1.AddBlazorWebView();          
             services1.AddSingleton<AppState>(_appState);
             Resources.Add("services1", services1.BuildServiceProvider());
 
